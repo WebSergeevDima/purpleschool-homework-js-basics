c8a8aa7037c6f97ef244631d9d57b3bf40b661d8
Сергеев Дмитрий <sergeev@tbm-technology.ru>
2024-05-15 00:15:47 +0300

    Исправил конвертер валют. Реализовал с одним switch

--- 7-converter/app.js
+++ 7-converter/app.js
@@ -1,56 +1,36 @@
-const converterTo$ = (money, cur, is$ = false) => is$ ? money * cur : money / cur;
-
 const converterToUserCur = (money, cur) => money * cur;
 
 const converter = (money, curStart, curFinish) => {
-    let usdMoney;
-    const RUB$ = 93.03;
-    const EUR$ = 0.94;
-    const BYN$ = 3.27;
-    const CZK$ = 23.73;
+    const RUB$ = 91.36;
+    const $RUB = 0.010946;
+    const EUR$ = 0.92965;
+    const $EUR = 1.08;
+    const EURRUB = 98.67;
+    const RUBEUR = 0.010135;
 
-    if(curStart === '$') {
-        switch (curFinish) {
-            case 'руб':
-                return converterTo$(money, RUB$, true);
-            case 'евро':
-                return converterTo$(money, EUR$, true);
-            case 'бел руб':
-                return converterTo$(money, BYN$, true);
-            case 'чешская крона':
-                return converterTo$(money, CZK$, true);
-        }
-    }
-
-    switch (curStart) {
-        case 'руб':
-            usdMoney = converterTo$(money, RUB$);
+    switch (curStart + curFinish) {
+        case 'руб$':
+            return converterToUserCur(money, $RUB);
             break;
-        case 'евро':
-            usdMoney = converterTo$(money, EUR$);
+        case '$руб':
+            return converterToUserCur(money, RUB$);
             break;
-        case 'бел руб':
-            usdMoney = converterTo$(money, BYN$);
+        case 'евро$':
+            return converterToUserCur(money, $EUR);
             break;
-        case 'чешская крона':
-            usdMoney = converterTo$(money, CZK$);
+        case '$евро':
+            return converterToUserCur(money, EUR$);
             break;
-        default: return null;
-    }
-
-    switch (curFinish) {
-        case 'руб':
-            return converterToUserCur(usdMoney, RUB$);
-        case 'евро':
-            return converterToUserCur(usdMoney, EUR$);
-        case 'бел руб':
-            return converterToUserCur(usdMoney, BYN$);
-        case 'чешская крона':
-            return converterToUserCur(usdMoney, CZK$);
-        case '$':
-            return usdMoney;
-        default: return null;
+        case 'евроруб':
+            return converterToUserCur(money, EURRUB);
+            break;
+        case 'рубевро':
+            return converterToUserCur(money, RUBEUR);
+            break;
+        default:
+            return 0;
     }
 }
 
-console.log(converter(1000, 'евро', 'руб'));
+//Вы можете использовать: руб, $, евро
+console.log(converter(1000, 'руб', 'евро'));
